ef84ea526655c8efc798bd38b89508bfc0815898
manu <manuboto5@gmail.com>
2024-10-30 12:35:27 +0100

    subida archivos peliculing

---
+++ AppWeb/web/public/javascripts/prueba.js
@@ -0,0 +1,7 @@
+function mostrarFormulario() {
+    document.getElementById('formulario').style.display = 'block';
+}
+
+function cerrarFormulario() {
+    document.getElementById('formulario').style.display = 'none';
+}
